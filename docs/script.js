94111f01e81efdc02cc3c8ea00ee3b9e85b976fc
Nicole Schilder <43071011+KabouterGeitje@users.noreply.github.com>
2024-02-12 11:01:28 +0100

    fix fix fix

--- docs/script.js
+++ docs/script.js
@@ -3,7 +3,7 @@ console.log("hey hoi");
 const naam = document.querySelector("h1");
 
 async function FetchData() {
-  const response = await fetch("./info.json");
+  const response = await fetch("./json/info.json");
   const data = await response.json();
 
   naam.innerHTML = data.naam;
