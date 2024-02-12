aaff6fbcc364e8fb66b7a57126bb3dc059cae452
Nicole Schilder <43071011+KabouterGeitje@users.noreply.github.com>
2024-02-12 14:51:06 +0100

    update

--- docs/script.js
+++ docs/script.js
@@ -1,4 +1,21 @@
-console.log("hey hoi");
+/*image hover */
+
+
+const avatar = document.querySelector('body > section:nth-of-type(1) > img')
+const h1 = document.querySelector('h1');
+
+function hoverActivate(){
+  h1.classList.add('scale');
+  
+}
+function hoverDeactivate(){
+  h1.classList.remove('scale');
+}
+
+avatar.addEventListener("mouseover", hoverActivate);
+avatar.addEventListener("mouseout", hoverDeactivate);
+
+/*krijg json info uit json bestand*/
 
 const naam = document.querySelector("h1");
 
